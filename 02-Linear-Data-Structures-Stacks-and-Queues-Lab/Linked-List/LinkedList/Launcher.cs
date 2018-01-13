fab370b9c31a020f9cb290f19a94febd2b027328
Jeliozver <jelioradoev@gmail.com>
2018-01-13 10:31:26 +0200

    Added solutions for Stacks and Queues lab tasks

---
+++ 02-Linear-Data-Structures-Stacks-and-Queues-Lab/Linked-List/LinkedList/Launcher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Launcher
+{
+    public static void Main()
+    {
+        LinkedList<int> list = new LinkedList<int>();
+
+        list.AddFirst(1);
+        list.AddLast(2);
+        list.AddFirst(3);
+        list.AddLast(4);
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine("Now removing first and last");
+
+        list.RemoveFirst();
+        list.RemoveLast();
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
---
+++ 02-Linear-Data-Structures-Stacks-and-Queues-Lab/Linked-List/LinkedList/LinkedList.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LinkedList<T> : IEnumerable<T>
+{
+    public class Node
+    {
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+        public Node Next { get; set; }
+    }
+
+    public Node Head { get; private set; }
+    public Node Tail { get; private set; }
+    public int Count { get; private set; }
+
+    public void AddFirst(T item)
+    {
+        Node old = Head;
+
+        Head = new Node(item);
+        Head.Next = old;
+
+        if (Count == 0)
+        {
+            Tail = Head;
+        }
+
+        Count++;
+    }
+
+    public void AddLast(T item)
+    {
+        Node old = Tail;
+        Tail = new Node(item);
+
+        if (Count == 0)
+        {
+            Head = Tail;
+        }
+        else
+        {
+            old.Next = Tail;
+        }
+
+        Count++;
+    }
+
+    public T RemoveFirst()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        T item = Head.Value;
+
+        if (Count == 1)
+        {
+            Head = null;
+            Tail = null;
+        }
+        else
+        {
+            Head = Head.Next;
+        }
+
+        Count--;
+        return item;
+    }
+
+    public T RemoveLast()
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        T item = Tail.Value;
+
+        if (Count == 1)
+        {
+            Head = null;
+            Tail = null;
+        }
+        else
+        {
+            Node parent = Head;
+
+            while (parent.Next != Tail)
+            {
+                parent = parent.Next;
+            }
+
+            parent.Next = null;
+            Tail = parent;
+        }
+
+        Count--;
+        return item;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        Node current = Head;
+
+        while (current != null)
+        {
+            yield return current.Value;
+            current = current.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
