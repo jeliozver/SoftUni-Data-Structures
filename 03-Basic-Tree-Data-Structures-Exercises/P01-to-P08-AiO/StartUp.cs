0a14df50bd76b2cd28adc2c7a8644affcc5ee71f
Jeliozver <jelioradoev@gmail.com>
2018-01-24 10:52:12 +0200

    Added solutions for BST exercises problems

---
+++ 03-Basic-Tree-Data-Structures-Exercises/P01-to-P08-AiO/StartUp.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class StartUp
+{
+    private static Dictionary<int, Tree<int>> nodeByValue = new Dictionary<int, Tree<int>>();
+
+    public static void Main()
+    {
+        ReadTree();
+        Tree<int> root = GetRootNode();
+
+        // P01-Root-Node (uncomment next line if you want to test in Judge) (100 / 100)
+        // Console.WriteLine($"Root node: {root.Value}");
+
+        // P02-Print-Tree (uncomment next line if you want to test in Judge) (100 / 100)
+        // root.PrintTree();
+
+        // PO3-Leaf-Nodes (uncomment next two lines if you want to test in Judge) (100 / 100)
+        // List<Tree<int>> leafNodes = root.GetLeafNodes();
+        // PrintLeafNodes(leafNodes);
+
+        // PO4-Middle-Nodes (uncomment next two lines if you want to test in Judge) (100 / 100)
+        // List<Tree<int>> middleNodes = root.GetMiddleNodes();
+        // PrintMiddleNodes(middleNodes);
+
+        // P05-Deepest-Node (uncomment next five lines if you want to test in Judge) (100 / 100)
+        // List<Tree<int>> nodesDeepness = root
+        //    .GetNodesDeepness()
+        //    .OrderByDescending(x => x.Deepness)
+        //    .ToList();
+        // Console.WriteLine($"Deepest node: {nodesDeepness[0].Value}");
+
+        // PO6-Longest-Path (uncomment next seven lines if you want to test in Judge) (100 / 100)
+        // List<Tree<int>> nodesDeepness = root
+        //   .GetNodesDeepness()
+        //   .OrderByDescending(x => x.Deepness)
+        //   .ToList();
+        // Tree<int> deepestNode = nodesDeepness[0];
+        // List<int> longestPath = deepestNode.GetPath();
+        // Console.WriteLine($"Longest path: {string.Join(" ", longestPath)}");
+
+        // P07-All-Paths-With-a-Given-Sum (uncomment next three lines if you want to test in Judge) (100 / 100)
+        // int sum = int.Parse(Console.ReadLine());
+        // List<Tree<int>> leafNodes = root.GetLeafNodes();
+        // PrintPathsOfSum(leafNodes, sum);
+
+        // P08-All-Subtrees-With-a-Given-Sum (uncomment next eight lines if you want to test in Judge) (60 / 100)
+        // int sum = int.Parse(Console.ReadLine());
+        // List<Tree<int>> middleNodes = new List<Tree<int>>();
+        // foreach (var node in nodeByValue.Values)
+        //{
+        //    middleNodes.AddRange(node.GetMiddleNodes());
+        //}
+        // var filter = middleNodes.Distinct().ToList();
+        // PrintSubTreesOfSum(filter, sum);
+    }
+
+    public static void ReadTree()
+    {
+        int nodeCout = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i < nodeCout; i++)
+        {
+            string[] edge = Console.ReadLine()
+                .Split(' ');
+
+            AddEdge(int.Parse(edge[0]), int.Parse(edge[1]));
+        }
+    }
+
+    public static void AddEdge(int parent, int child)
+    {
+        Tree<int> parentNode = GetNodeByValue(parent);
+        Tree<int> childNode = GetNodeByValue(child);
+
+        parentNode.Children.Add(childNode);
+        childNode.Parent = parentNode;
+    }
+
+    public static Tree<int> GetNodeByValue(int value)
+    {
+        if (!nodeByValue.ContainsKey(value))
+        {
+            nodeByValue[value] = new Tree<int>(value);
+        }
+
+        return nodeByValue[value];
+    }
+
+    public static Tree<int> GetRootNode()
+    {
+        return nodeByValue.Values
+            .FirstOrDefault(x => x.Parent == null);
+    }
+
+    public static void PrintLeafNodes(List<Tree<int>> leafNodes)
+    {
+        var sb = new StringBuilder();
+
+        foreach (var leaf in leafNodes.OrderBy(x => x.Value))
+        {
+            sb.Append($"{leaf.Value} ");
+        }
+
+        Console.WriteLine($"Leaf nodes: {sb.ToString().TrimEnd()}");
+    }
+
+    public static void PrintMiddleNodes(List<Tree<int>> middleNodes)
+    {
+        var sb = new StringBuilder();
+
+        foreach (var leaf in middleNodes.OrderBy(x => x.Value))
+        {
+            sb.Append($"{leaf.Value} ");
+        }
+
+        Console.WriteLine($"Middle nodes: {sb.ToString().TrimEnd()}");
+    }
+
+    public static void PrintPathsOfSum(List<Tree<int>> leafNodes, int sum)
+    {
+        Console.WriteLine($"Paths of sum {sum}:");
+
+        foreach (var node in leafNodes)
+        {
+            List<int> nodePath = node.GetPath();
+
+            if (nodePath.Sum() == sum)
+            {
+                Console.WriteLine(string.Join(" ", nodePath));
+            }
+        }
+    }
+
+    public static void PrintSubTreesOfSum(List<Tree<int>> middleNodes, int sum)
+    {
+        Console.WriteLine($"Subtrees of sum {sum}:");
+
+        foreach (var node in middleNodes)
+        {
+            var sb = new StringBuilder();
+            int currentSum = node.Value;
+            sb.Append($"{node.Value} ");
+
+            foreach (var child in node.Children)
+            {
+                sb.Append($"{child.Value} ");
+                currentSum += child.Value;
+            }
+
+            if (currentSum == sum)
+            {
+                Console.WriteLine($"{sb.ToString().TrimEnd()}");
+            }
+        }
+    }
+}
---
+++ 03-Basic-Tree-Data-Structures-Exercises/P01-to-P08-AiO/Tree.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Tree<T>
+{
+    public T Value { get; set; }
+    public Tree<T> Parent { get; set; }
+    public List<Tree<T>> Children { get; set; }
+    public int Deepness { get; private set; }
+
+    public Tree(T value, params Tree<T>[] children)
+    {
+        this.Value = value;
+        this.Children = new List<Tree<T>>();
+
+        foreach (var child in children)
+        {
+            this.Children.Add(child);
+            child.Parent = this;
+            child.Deepness = 0;
+        }
+    }
+
+    public void PrintTree(int indent = 0)
+    {
+        this.PrintTree(this, indent);
+    }
+
+    private void PrintTree(Tree<T> node, int indent)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        Console.WriteLine($"{new string(' ', indent)}{node.Value}");
+
+        foreach (var child in node.Children)
+        {
+            child.PrintTree(indent + 2);
+        }
+    }
+
+    public List<Tree<T>> GetLeafNodes()
+    {
+        var leafNodes = new List<Tree<T>>();
+
+        this.GetLeafNodesRec(this, leafNodes);
+
+        return leafNodes;
+    }
+
+    private void GetLeafNodesRec(Tree<T> node, List<Tree<T>> leafNodes)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        if (node.Children.Count == 0)
+        {
+            leafNodes.Add(node);
+        }
+
+        foreach (var child in node.Children)
+        {
+            child.GetLeafNodesRec(child, leafNodes);
+        }
+    }
+
+    public List<Tree<T>> GetMiddleNodes()
+    {
+        var middleNodes = new List<Tree<T>>();
+
+        this.GetMiddleNodesRec(this, middleNodes);
+
+        return middleNodes;
+    }
+
+    private void GetMiddleNodesRec(Tree<T> node, List<Tree<T>> middleNodes)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        if (node.Parent != null && node.Children.Count != 0)
+        {
+            middleNodes.Add(node);
+        }
+
+        foreach (var child in node.Children)
+        {
+            child.GetMiddleNodesRec(child, middleNodes);
+        }
+    }
+
+    public List<Tree<T>> GetNodesDeepness(int deepness = 0)
+    {
+        var result = new List<Tree<T>>();
+
+        this.GetNodesDeepnessRec(this, deepness, result);
+
+        return result;
+    }
+
+    private void GetNodesDeepnessRec(Tree<T> node, int deepness, List<Tree<T>> result)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        node.Deepness = deepness;
+        result.Add(node);
+
+        foreach (var child in node.Children)
+        {
+            child.GetNodesDeepnessRec(child, deepness + 1, result);
+        }
+    }
+
+    public List<T> GetPath()
+    {
+        var result = new List<T>();
+        var queue = new Queue<Tree<T>>();
+        queue.Enqueue(this);
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+            result.Add(current.Value);
+
+            if (current.Parent != null)
+            {
+                queue.Enqueue(current.Parent);
+            }
+        }
+
+        result.Reverse();
+        return result;
+    }
+}
