ebe2553b36c2031730e84761d715a29092789994
Jeliozver <jelioradoev@gmail.com>
2018-03-05 08:53:58 +0200

    Added solutions for heaps and priority queues lab problems

---
+++ 05-Heaps-Priority-Queues-Lab/Max-Binary-Heap-Heap-Sort/BinaryHeap/Heap.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public static class Heap<T> where T : IComparable<T>
+{
+    public static void Sort(T[] arr)
+    {
+        ConstructHeap(arr);
+
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            Swap(arr, 0, i);
+            Down(arr, 0, i);
+        }
+    }
+
+    private static void ConstructHeap(T[] arr)
+    {
+        for (int i = arr.Length / 2; i >= 0; i--)
+        {
+            Down(arr, i, arr.Length);
+        }
+    }
+
+    private static void Down(T[] arr, int index, int lenght)
+    {
+        int parentIndex = index;
+
+        while (parentIndex < lenght / 2)
+        {
+            int childIndex = parentIndex * 2 + 1;
+
+            if (childIndex + 1 < lenght
+                && IsGreater(arr, childIndex, childIndex + 1))
+            {
+                childIndex++;
+            }
+
+            int compare = arr[parentIndex]
+                .CompareTo(arr[childIndex]);
+
+            if (compare < 0)
+            {
+                Swap(arr, childIndex, parentIndex);
+            }
+
+            parentIndex = childIndex;
+        }
+    }
+
+    private static bool IsGreater(T[] arr, int left, int right)
+    {
+        return arr[left]
+                   .CompareTo(arr[right]) < 0;
+    }
+
+    private static void Swap(T[] arr, int parent, int index)
+    {
+        T temp = arr[parent];
+        arr[parent] = arr[index];
+        arr[index] = temp;
+    }
+}
---
+++ 05-Heaps-Priority-Queues-Lab/Max-Binary-Heap-Heap-Sort/BinaryHeap/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Console.WriteLine("Created an empty heap.");
+        BinaryHeap<int> heap = new BinaryHeap<int>();
+        heap.Insert(5);
+        heap.Insert(8);
+        heap.Insert(1);
+        heap.Insert(3);
+        heap.Insert(12);
+        heap.Insert(-4);
+
+        Console.WriteLine("Heap elements (max to min):");
+        while (heap.Count > 0)
+        {
+            int max = heap.Pull();
+            Console.WriteLine(max);
+        }
+
+        int[] arr = { 5, 2, 0, -4, 3, 12 };
+        Console.WriteLine($"Unsorted: {string.Join(" ", arr)}");
+
+        Heap<int>.Sort(arr);
+        Console.WriteLine($"Sorted: {string.Join(" ", arr)}");
+    }
+}
